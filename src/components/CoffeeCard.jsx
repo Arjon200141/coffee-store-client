aa5131109231b7cb6fd93f79087879e7f8c4317a
Arjon200141 <aorjon123@gmail.com>
2024-05-06 15:15:54 +0600

    Delete Added

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,12 +1,42 @@
 import { CiEdit } from "react-icons/ci";
 import { GrView } from "react-icons/gr";
 import { MdDeleteForever } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
     const { _id, name, chef, taste, category, details, photo } = coffee;
 
-    const handleDelete = _id =>{
+    const handleDelete = _id => {
         console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                console.log('Delete Confirmed');
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE',
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletdCount > 0) {
+                              Swal.fire({
+                                title: "Deleted!",
+                                text: "Your coffee has been deleted.",
+                                icon: "success"
+                              });
+                        }
+                    })
+            }
+        });
     }
 
     return (
@@ -26,7 +56,7 @@ const CoffeeCard = ({ coffee }) => {
                                 <div className="flex flex-col space-y-3 ">
                                     <button className="btn join-item font-bold  text-white rounded-md bg-[#D2B48C]"><GrView /></button>
                                     <button className="btn join-item font-bold text-white rounded-md bg-[#3C393B]"><CiEdit /></button>
-                                    <button className="btn join-item font-bold text-white rounded-md bg-[#EA4744]" onClick={()=> handleDelete(_id)}><MdDeleteForever /></button>
+                                    <button className="btn join-item font-bold text-white rounded-md bg-[#EA4744]" onClick={() => handleDelete(_id)}><MdDeleteForever /></button>
                                 </div>
                             </div>
                         </div>
